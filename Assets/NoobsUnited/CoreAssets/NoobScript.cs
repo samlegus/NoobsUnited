ee50fd584d4d7a76897864073f94e42a606695bc
samuelLegus <samuel.legus@live.com>
2015-06-03 23:34:29 -0700

    Started optional debug info feature

--- Assets/NoobsUnited/CoreAssets/NoobScript.cs
+++ Assets/NoobsUnited/CoreAssets/NoobScript.cs
@@ -1,10 +1,39 @@
 ﻿using UnityEngine;
+using UnityEditor;
 using System.Collections;
+using NoobsUnited;
 
 public class NoobScript : MonoBehaviour 
 {
+	public bool DebugTextInfo = false;
+	public bool DebugPhysicsGizmos = false;
+	
 	public GameObject self 
 	{
 		get { return gameObject;}
+	}
+	
+	public static GameObject Clone(GameObject gameObj, Vector2 position)
+	{
+		return Instantiate (gameObj, position, Quaternion.identity) as GameObject;
+	}
+
+	public static GameObject Clone(GameObject gameObj, Vector2 position, Quaternion rotation)
+	{
+		return Instantiate (gameObj, position, rotation) as GameObject;
+	}
+	
+	void OnGUI()
+	{
+		Vector3 offsetPositionWorldSpace = transform.position;//- (Vector3.down * (transform.localScale.y / 2f));
+		offsetPositionWorldSpace.y *= -1f;
+		Vector3 positionInScreenSpace = Camera.main.WorldToScreenPoint (offsetPositionWorldSpace);
+		GUI.Label(	new Rect(	positionInScreenSpace.x, positionInScreenSpace.y, 150, 30),
+		          				gameObject.name + "( " + transform.position.x + " , " + transform.position.y + " )" );
+	}
+
+	void OnDrawGizmos()
+	{
+		Gizmos.DrawWireSphere (transform.position, transform.localScale.x);
 	}
 }
