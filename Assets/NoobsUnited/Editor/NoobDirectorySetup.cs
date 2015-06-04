b3ba4e798efb15070d0e838dd09c4eba7496ca43
samuelLegus <samuel.legus@live.com>
2015-06-03 22:48:12 -0700

    Added noob directory setup to menu bar

---
+++ Assets/NoobsUnited/Editor/NoobDirectorySetup.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using System.IO;
+
+public class NoobDirectorySetup 
+{
+	
+	[MenuItem("Noobs United/Setup Noob Directories")]
+	private static void OpenMonoWizardFromContextMenu()
+	{
+		AssetDatabase.CreateFolder ("Assets", "MyGame");
+		AssetDatabase.CreateFolder ("Assets/MyGame", "Scripts");
+		AssetDatabase.CreateFolder ("Assets/MyGame", "Prefabs");
+		AssetDatabase.CreateFolder ("Assets/MyGame", "ArtAssets");
+		AssetDatabase.CreateFolder ("Assets/MyGame", "Scenes");
+
+		AssetDatabase.Refresh ();
+	}
+	
+	[MenuItem("Noobs United/Setup Noob Directories", true)]
+	private static bool ValidateMonoWizardFromContextMenu()
+	{
+		return !(Directory.Exists (Application.dataPath + "/MyGame"));
+	}
+
+}
