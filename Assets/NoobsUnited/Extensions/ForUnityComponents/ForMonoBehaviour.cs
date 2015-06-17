50f5a932be218f4d543a5334cfd1b4082a222294
blueskies9041 <sam.legus@gmail.com>
2015-06-17 09:31:35 -0700

    More clone overloads added

--- Assets/NoobsUnited/Extensions/ForUnityComponents/ForMonoBehaviour.cs
+++ Assets/NoobsUnited/Extensions/ForUnityComponents/ForMonoBehaviour.cs
@@ -5,10 +5,35 @@ using NoobsUnited;
 namespace NoobsUnited
 {
 	public static partial class UnityExtensions
-	{
+	{	
 		public static GameObject Clone(this MonoBehaviour script, GameObject obj, Vector2 position)
 		{
 			return GameObject.Instantiate(obj, position, Quaternion.identity) as GameObject;
+		}
+		
+		public static GameObject Clone(this MonoBehaviour script, GameObject obj, Vector2 position, Quaternion rotation)
+		{
+			return GameObject.Instantiate(obj, position, rotation) as GameObject;
+		}
+		
+		public static GameObject Clone(this MonoBehaviour script, GameObject obj, Vector2 position, bool ignoreOwnCollider)
+		{
+			GameObject clone = GameObject.Instantiate(obj, position, Quaternion.identity) as GameObject;
+			if(ignoreOwnCollider)
+			{
+				Physics2D.IgnoreCollision (script.gameObject.GetComponent<Collider2D>(), clone.GetComponent<Collider2D>());
+			}
+			return clone;
+		}
+		
+		public static GameObject Clone(this MonoBehaviour script, GameObject obj, Vector2 position, Quaternion rotation, bool ignoreOwnCollider)
+		{
+			GameObject clone = GameObject.Instantiate(obj, position, Quaternion.identity) as GameObject;
+			if(ignoreOwnCollider)
+			{
+				Physics2D.IgnoreCollision (script.gameObject.GetComponent<Collider2D>(), clone.GetComponent<Collider2D>());
+			}
+			return clone;
 		}
 	}
 }
