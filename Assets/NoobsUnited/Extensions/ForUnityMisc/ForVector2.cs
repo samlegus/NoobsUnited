9e012ad7b0c181814f057acf8684efc79a6b3cfe
blueskies9041 <sam.legus@gmail.com>
2015-06-17 10:27:30 -0700

    Vector2 heading extension added

---
+++ Assets/NoobsUnited/Extensions/ForUnityMisc/ForVector2.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using NoobsUnited;
+
+namespace NoobsUnited
+{
+	public static partial class UnityExtensions
+	{
+		public static Vector2 GetHeading(this Vector2 v, Vector2 from, Vector2 to )
+		{
+			return (to - from).normalized;
+		}
+	}
+}
